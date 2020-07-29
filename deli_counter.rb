1cdba47302ca2074511cd6c95b399dba4214e04f
secure-revert-8990 <yuliyambg@gmail.com>
2020-07-29 22:01:24 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+
+def line(array)
+  if array==[]
+   puts "The line is currently empty."
+  else 
+    namelist =""
+    array.each_with_index do |person,i|
+      namelist << " #{(i+1)}. #{person}"
+    end
+    puts "The line is currently:" + namelist
+  end
+end 
+
+
+
+def take_a_number(line,newName)
+ line.push(newName)
+ puts "Welcome, #{newName}. You are number #{line.length} in line."
+end
+
+def now_serving(array)
+  if array.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array[0]}."
+    array.shift
+  end
+end
