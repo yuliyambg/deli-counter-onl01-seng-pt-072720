0221f798ec6e09b0dddadd8550be32fb5384a7ea
secure-revert-8990 <yuliyambg@gmail.com>
2020-07-29 21:37:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+
+def line(array)
+  if array==[]
+   puts "The line is currently empty."
+  else 
+    namelist =""
+    array.each_with_index do |person,i|
+      namelist << " #{(i+1)}. #{person}"
+    end
+    puts "The line is currently:" + namelist
+  end
+end 
